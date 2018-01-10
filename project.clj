a0434a56fa298fb3afe855520e0cbcbd5be12e4f
Josh Lehman <jalehman37@gmail.com>
2018-01-10 15:23:42 -0800

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/toolbelt-datomic "0.2.0-SNAPSHOT"
+(defproject starcity/toolbelt-datomic "0.2.0"
   :description "Utility functinos for working with datomic."
   :url "https://github.com/starcity-properties/toolbelt-datomic"
   :license {:name "Eclipse Public License"
