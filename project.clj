0fa0a165d49fb7fa928027ec24ed82dbb0fce27d
Josh Lehman <jalehman37@gmail.com>
2018-01-10 14:23:41 -0800

    fix releases

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [com.datomic/datomic-free "0.9.5544" :scope "provided"]]
-  :deploy-repositories [["releases" :clojars]])
+  :deploy-repositories [["releases" {:url   "https://clojars.org/repo"
+                                     :creds :gpg}]])
