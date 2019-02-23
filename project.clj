9507cb36f09c4d16e3a1276242d49a076e824386
Diana Negma <dianagren@gmail.com>
2019-02-22 16:48:34 -0800

    Version 0.6.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/toolbelt-datomic "0.6.2-SNAPSHOT"
+(defproject starcity/toolbelt-datomic "0.6.2"
   :description "Utility functinos for working with datomic."
   :url "https://github.com/starcity-properties/toolbelt-datomic"
   :license {:name "Eclipse Public License"
